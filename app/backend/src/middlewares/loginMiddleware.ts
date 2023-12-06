c939e7c5c57f432b10d96449cc43634d42067494
João José <joaojose@Joaos-MacBook-Pro.local>
2023-12-06 15:20:33 -0300

    req. 10

--- app/backend/src/middlewares/loginMiddleware.ts
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -8,6 +8,13 @@ class LoginMiddleware {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
     // Adicione outras validações, se necessário
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    if (password.length < 6) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
     next();
   }
 }
