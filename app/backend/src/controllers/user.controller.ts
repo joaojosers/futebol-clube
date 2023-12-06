de9fb20110709197a1f004a1afa1c3c248b37382
João José <joaojose@Joaos-MacBook-Pro.local>
2023-12-06 16:42:46 -0300

    debugging req. 10

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -10,7 +10,7 @@ export default class UserController {
     const { email, password } = req.body;
     const token = await this._userService.login({ email, password });
 
-    if (!token) return res.status(401).json({ message: 'Incorrect email or password' });
+    if (!token) return res.status(401).json({ message: 'Invalid email or password' });
 
     return res.status(200).json({ token });
   }
